4550c23b7553f37762b4c124ecb28518e478c231
RanggaPutraArdana <88466519+RanggaPutraArdana@users.noreply.github.com>
2021-08-08 19:14:43 +0700

    Add files via upload

---
+++ db26_rangga_putra_ardana.sql
@@ -0,0 +1,38 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 08 Agu 2021 pada 12.40
+-- Versi server: 10.4.20-MariaDB
+-- Versi PHP: 7.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db26_rangga putra ardana`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `db26_rangga putra ardana`
+--
+
+CREATE TABLE `db26_rangga putra ardana` (
+  `Nama siswa` varchar(20) NOT NULL,
+  `No absen` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
